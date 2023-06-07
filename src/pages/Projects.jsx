01ca4285220dc011fb4c3b85e63e55351aaef006
Jordan Aston <gcab022124@coderacademy.edu.au>
2023-06-07 23:25:53 +1000

    more props added to projects

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -4,30 +4,45 @@ import MobileMenu from '../components/MobileMenu';
 import TopNavBar from '../components/TopNavBar';
 import ProLinks from '../components/ProLinks';
 import ProjectTile from '../components/ProjectTile';
+import PlantAppImage from '../assets/plantapp-image.png';
+import VinylDataImage from '../assets/vinyl-data-image.png';
+
 
 function Projects({ isDarkMode }) {
-  const projectText = [
+  const projectItems = [
     {
       details: '2023 / Coder Academy',
-      description: 'Van Voyage - Campervan Hire',
-      stack: 'MERN Stack / Tailwind'
+      description: 'Full Stack Application',
+      stack: 'MERN Stack | Tailwind',
+      image: ''
     },
     {
       details: '2023 / Personal Project',
-      description: 'Music Player App (In Production)',
-      stack: 'Javascript / React / Tailwind'
+      description: 'Music Player App',
+      stack: 'Javascript | React | Tailwind',
+      image: ''
     },
     {
       details: '2023 / Coder Academy',
-      description: 'Vinyl Data API - Postman',
-      stack: 'Python / Flask / PostgreSQL'
+      description: 'Vinyl Data API - API Webserver',
+      stack: 'Python | Flask | PostgreSQL',
+      image: VinylDataImage,
     },
     {
       details: '2022 / Coder Academy',
-      description: 'Terminal Application - PlantApp.',
-      stack: 'Python'
+      description: 'PlantApp - Terminal Application',
+      stack: 'Python',
+      image: PlantAppImage,
     },
+    // {
+    //   details: '2023 / Coder Academy',
+    //   description: 'Moonswell - Hackathon App',
+    //   stack: 'Javascript | React | Tailwind',
+    //   image: '',
+    // },
   ];
+
+  let textColor = isDarkMode ? 'text-dark-mode-color' : 'text-light-mode-color';
 
   return (
     <div className="flex flex-col justify-center items-center h-screen">
@@ -42,14 +57,16 @@ function Projects({ isDarkMode }) {
       </div>
       <div className="flex flex-col items-center overflow-y-auto pt-16 pb-24">
         <div className="container mx-auto h-full max-h-full overflow-y-auto">
+          <h1 className={`text-center text-xl font-roboto-mono font-light my-5 ${textColor}`}>Recent Work</h1>
           <div className="flex flex-wrap justify-center items-center">
-            {projectText.map((project, index) => (
+            {projectItems.map((project, index) => (
               <ProjectTile
                 key={index}
                 isDarkMode={isDarkMode}
                 projectDetails={project.details}
                 projectDescription={project.description}
                 techStack={project.stack}
+                projectImage={project.image}
               />
             ))}
           </div>
