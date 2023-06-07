2c79a008b3269cdaaaf49c33bf49e986daea22ce
Jordan Aston <gcab022124@coderacademy.edu.au>
2023-06-07 23:25:02 +1000

    projecttile.jsx built out further with conditional rendering to handle projects in still in development

--- src/components/ProjectTile.jsx
+++ src/components/ProjectTile.jsx
@@ -1,13 +1,19 @@
 import React from 'react';
 
-function ProjectTile({ projectDescription, projectDetails, techStack, isDarkMode }) {
+function ProjectTile({ projectDescription, projectDetails, techStack, projectImage, isDarkMode }) {
   let textColor = isDarkMode ? 'text-dark-mode-color' : 'text-light-mode-color';
-  let borderColor = isDarkMode ? 'border-dark-mode-color' : 'border-light-mode-color';
+  let borderColor = (isDarkMode ? 'border-dark-mode-color' : 'border-light-mode-color');
 
   return (
     <div>
-      <div className={`flex project-tile m-4 w-80 h-60 bg-none border-2 items-center justify-center font-roboto font-light ${borderColor} relative`}>
-          <p className={textColor}>Project</p>
+      <div className={`flex project-tile m-4 w-80 h-60 items-center justify-center relative`}>
+        {projectImage ? (
+          <img src={projectImage} alt="Project Image" />
+        ) : (
+          <div className={`flex items-center w-80 h-60 border-2 ${borderColor} font-roboto font-regular justify-center ${textColor}`}>
+            <p>In Development...</p>
+          </div>
+        )}
       </div>
       <div className="font-roboto font-light">
             <p className={`m-5 ${textColor}`}>{projectDetails}</p>
@@ -19,6 +25,8 @@ function ProjectTile({ projectDescription, projectDetails, techStack, isDarkMode
 }
 
 export default ProjectTile;
+
+
 
 
 
